f3fc63e95c79fa84e6bf75b6388322a1bec281e1
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-09-14 20:39:01 +0000

    program add

---
+++ rectangle_area_perimeter.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 Wajd Mariam All rights reserved.
+//
+// Created by: Wajd Mariam
+// Created on:September 2019
+// This program calculates the area and the perimeter of the rectangle
+
+#include <iostream>
+
+int main() {
+    std::cout << "If the rectangle's dimensions are:" << std::endl;
+    std::cout << "5m long and 3m wide " << std::endl;
+    std::cout << "Area is " << (5*3) << " m^2" << std::endl;
+    std::cout << "Perimeter is " << (2*(5+3)) << "m" << std::endl;
+}
